f498d2ec85b3ce1541275b2419bd5a8883cbd533
David Yepes Buitrago <david.yepes.ext@itau.co>
2019-12-02 14:53:02 -0500

    Base Project

---
+++ PlatziFinanzasIPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzasIPhone
+//
+//  Created by David Yepes Buitrago on 12/2/19.
+//  Copyright © 2019 David Yepes Buitrago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
